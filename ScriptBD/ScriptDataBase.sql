8a872c578a2e79e134280fa6e68df9d51ef23ea7
Isamuna Nkembo Josue <josamuna2009@gmail.com>
2019-05-11 12:55:22 +0200

    Finalise Database Script and stored procedures

---
+++ ScriptBD/ScriptDataBase.sql
@@ -0,0 +1,58 @@
+--drop database gestion_personne
+--go
+
+create database gestion_personne
+go
+
+use gestion_personne
+go
+
+create table personne
+(
+	id int,
+	nom varchar(50) not null,
+	postnom varchar(50),
+	prenom varchar(50),
+	sexe varchar(1) default 'M',
+	constraint pk_personne primary key(id),
+	constraint uk_personne unique(nom,postnom,prenom)
+)
+go
+
+create table telephone 
+(
+	id int,
+	id_proprietaire int not null,
+	initial varchar(4) not null,
+	numero varchar(9) not null,
+	constraint pk_telephone primary key(id),
+	constraint fk_personne_telephone 
+	foreign key(id_proprietaire) references personne(id)
+)
+go
+
+create table adresse
+(
+	id int,
+	quartier varchar(50),
+	commune varchar(50),
+	ville varchar(50),
+	pays varchar(50) not null,
+	constraint pk_adresse primary key(id)
+)
+go
+
+create table domicile
+(
+	id int,
+	id_personne int not null,
+	id_adresse int not null,
+	avenue varchar(50),
+	numero_avenue int,
+	constraint pk_domicile primary key(id),
+	constraint fk_personne_domicile foreign key(id_personne)
+	references personne(id),
+	constraint fk_addresse_domicile foreign key(id_adresse)
+	references adresse(id)
+)
+go
---
+++ ScriptBD/StoredProcedure.sql
@@ -0,0 +1,128 @@
+--Insert or Update personne
+create procedure sp_insert_personne
+	@id int,@nom varchar(50),@postnom varchar(50),
+	@prenom varchar(50),@sexe varchar(1)
+as
+begin
+	if not exists(select * from personne where id=@id)
+		insert into personne(id,nom,postnom,prenom,sexe) values 
+		(@id,@nom,@postnom,@prenom,@sexe)
+	else
+		update personne set nom=@nom,postnom=@postnom,prenom=@prenom,
+		sexe=@sexe where id=@id
+end
+go
+
+--Delete personne
+create procedure sp_delete_personne
+	@id int
+as
+begin
+	if exists(select * from personne where id=@id)
+		delete from personne where id=@id
+end
+go
+
+--Select all personne
+create procedure sp_select_personnes
+as
+begin 
+	select id,nom,postnom,prenom,sexe from personne
+end
+go
+
+--Select one personne
+create procedure sp_select_personne
+	@id int
+as
+begin
+	select id,nom,postnom,prenom,sexe from personne 
+	where id=@id
+end
+go
+
+--Insert or Update telephone
+create procedure sp_insert_telephone
+	@id int,@id_proprietaire int,@initial varchar(4),
+	@numero varchar(9)
+as
+begin
+	if not exists(select * from telephone where id=@id)
+		insert into telephone(id,id_proprietaire,initial,numero) values 
+		(@id,@id_proprietaire,@initial,@numero)
+	else
+		update telephone set id_proprietaire=@id_proprietaire,
+		initial=@initial,numero=@numero where id=@id
+end
+go
+
+--Delete telephone
+create procedure sp_delete_telephone
+	@id int
+as
+begin
+	if exists(select * from telephone where id=@id)
+		delete from telephone where id=@id
+end
+go
+
+--Select all telephone
+create procedure sp_select_telephones
+as
+begin
+	select id,id_proprietaire,initial,numero from telephone
+end
+go
+
+--Select all telephone of personne
+create procedure sp_select_telephones_personne
+	@id_personne int	
+as
+begin
+	select id,id_proprietaire,initial,numero
+	from telephone where id_proprietaire=@id_personne
+end
+go
+
+--Select one telephone
+create procedure sp_select_telephone
+	@id int
+as
+begin
+	select id,id_proprietaire,initial,numero from telephone
+	where id=@id
+end
+go
+
+--Test Insert and Update
+exec sp_insert_personne 1,'Isamuna','Nkembo','Josue','M'
+exec sp_insert_personne 2,'Mataba','Kyole','Stephanie','F'
+exec sp_insert_personne 3,'Arakandi','sefu','Eva','M'
+select * from personne
+
+exec sp_insert_telephone 1,1,'+250','785623146'
+exec sp_insert_telephone 2,1,'+243','850001263'
+exec sp_insert_telephone 3,2,'+243','985645235'
+exec sp_insert_telephone 4,3,'+243','815790584'
+exec sp_insert_telephone 5,3,'+242','808256231'
+select * from telephone
+
+--Test delete
+--exec sp_delete_telephone 1
+--select * from telephone
+
+--exec sp_delete_personne 3
+--select * from personne
+
+--Test Select all
+exec sp_select_personnes
+
+exec sp_select_telephones
+
+--Test Select One
+exec sp_select_personne 3
+
+exec sp_select_telephone 4
+
+exec sp_select_telephones_personne 2
+
